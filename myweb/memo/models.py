65977f93a50ed2c63cb9dd3f3894f86e9e34f7ca
ian-yoon <quizoian20@gmail.com>
2022-01-25 11:48:16 +0900

    delete

---
+++ myweb/memo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from datetime import datetime
+
+class Memo(models.Model):
+    idx = models.AutoField(primary_key=True)
+    writer = models.TextField(null=False)
+    memo = models.TextField(null=False)
+    post_date = models.DateTimeField(default=datetime.now, blank=True)
